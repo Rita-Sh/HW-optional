3f6153c69926e9410fdc378ecdb13ee6389524d1
Rita-Sh <61317949+Rita-Sh@users.noreply.github.com>
2020-05-10 23:20:25 +0300

    Create Post_request.py

---
+++ Post_request.py
@@ -0,0 +1,17 @@
+import request
+async def login(request):
+
+    if request.method == 'POST':
+        form = await request.post()
+        error = validate_login(form)
+        if error:
+            return {'error': error}
+        else:
+            location = request.app.router['index'].url_for()
+            raise web.HTTPFound(location=location)
+
+    return {}
+
+app.router.add_get('/', index, name='index')
+app.router.add_get('/login', login, name='login')
+app.router.add_post('/login', login, name='login')
